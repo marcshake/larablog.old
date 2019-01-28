57e7156c11b5b11c46081c922964b715794b3d27
Marcel Schindler <m.schindler@kombifracht.de>
2019-01-28 09:52:59 +0100

    Refactored to use skeleton-scss instead of plain css

--- resources/views/larablog.blade.php
+++ resources/views/larablog.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/skeleton.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
 </head>
 <body>
     <div class="container">
